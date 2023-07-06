6aa5fbe42cbaec0cdb6cfbf50a38329f6bcfac43
unknown <you@example.com>
2023-07-06 14:22:51 +0100

    OC: Added Work

---
+++ script.js
@@ -0,0 +1,41 @@
+import chalk from 'chalk';
+// console.log(chalk.blue("helloooo world"))
+// console.log(chalk.bgWhiteBright("helloooo world"))
+// console.log(chalk.bgWhiteBright.red("helloooo world"))
+
+// for (let i = 1; i <=100; i++){
+//    if (i%2 === 0){
+//     console.log(chalk.blue(i))
+//    }
+//    else{
+//     console.log(chalk.red(i))
+//    }
+// }
+
+// for ( let j=0; j<=100; j++)
+// {
+//     if (j%3===0)
+//     {
+//         console.log("fizz", + j)
+//     }
+//     else if (j%5===0)
+//     {
+//         console.log("buzz" , + j)
+//     }
+//     else if (j%7===0)
+//     {
+//         console.log(chalk.blue("Zing" , + j))
+//     }
+//     else if (j%3!=0 && j%5!=0 && j%7!=0 )
+//     {
+//        console.log("It's not multiple a of 3,5,7" , + j)
+//     }
+// }
+
+console.log("GAME OF ROCK PAPER SCISSOR")
+
+var userChoise = prompt("Choose rock or paper or scissor")
+
+
+console.log("User choice" + userChoise);
+
